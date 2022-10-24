78ded1bce2044ca4ee72e247a9dd231e8ef4ca58
pokixrol <kaja.ricna@seznam.cz>
2022-10-24 17:52:20 +0200

    Update Test - zkouška SPIFFS - nefunguje

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -73,12 +73,13 @@ void loop()
  lcd.clear();
  lcd.setCursor (0,0);
  lcd.print("Hello World!");
+ delay(500);
 
 }*/
 
 //BME280
 
-#include <Wire.h>
+/*#include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
@@ -114,7 +115,7 @@ void loop() {
 
 	Serial.println();
 	delay(1000);
-}
+}*/
 
 //Hokus pokus s displayem
 
@@ -227,3 +228,142 @@ void setup()
  
     delay(2000);      // thingspeak needs minimum 15 sec delay between updates.
 }*/
+
+#include <AsyncEventSource.h>
+#include <AsyncPrinter.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FS.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+Adafruit_BME280 bme; // I2C
+//Adafruit_BME280 bme(BME_CS); // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+
+// Replace with your network credentials
+const char* ssid = "Ricny";
+const char* password = "karol111";
+
+// Set LED GPIO
+const int ledPin = 2;
+// Stores LED state
+String ledState;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+String getTemperature() {
+  float temperature = bme.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float temperature = 1.8 * bme.readTemperature() + 32;
+  Serial.println(temperature);
+  return String(temperature);
+}
+  
+String getHumidity() {
+  float humidity = bme.readHumidity();
+  Serial.println(humidity);
+  return String(humidity);
+}
+
+String getPressure() {
+  float pressure = bme.readPressure()/ 100.0F;
+  Serial.println(pressure);
+  return String(pressure);
+}
+
+// Replaces placeholder with LED state value
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if(digitalRead(ledPin)){
+      ledState = "ON";
+    }
+    else{
+      ledState = "OFF";
+    }
+    Serial.print(ledState);
+    return ledState;
+  }
+  else if (var == "TEMPERATURE"){
+    return getTemperature();
+  }
+  else if (var == "HUMIDITY"){
+    return getHumidity();
+  }
+  else if (var == "PRESSURE"){
+    return getPressure();
+  }  
+}
+ 
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  // Initialize the sensor
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  // Initialize SPIFFS
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  // Route to set GPIO to HIGH
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, HIGH);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to set GPIO to LOW
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, LOW);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", getTemperature().c_str());
+  });
+  
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", getHumidity().c_str());
+  });
+  
+  server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", getPressure().c_str());
+  });
+
+  // Start server
+  server.begin();
+}
+ 
+void loop(){
+  
+}
