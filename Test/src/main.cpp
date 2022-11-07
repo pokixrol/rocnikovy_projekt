04f41cbca4ea2f5797b897cc5ccb5c582b287ac1
pokixrol <kaja.ricna@seznam.cz>
2022-11-07 17:26:00 +0100

    Update test - pokus o připojení esp k wifi - nefunguje

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -229,7 +229,7 @@ void setup()
     delay(2000);      // thingspeak needs minimum 15 sec delay between updates.
 }*/
 
-#include <AsyncEventSource.h>
+/*#include <AsyncEventSource.h>
 #include <AsyncPrinter.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
@@ -366,4 +366,161 @@ void setup(){
  
 void loop(){
   
+}*/
+
+/*#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; 
+
+const char* ssid     = "Ricny";
+const char* password = "karol111";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+void setup() {
+  Serial.begin(115200);
+  bool status;
+
+  // default settings
+  //status = bme.begin();  
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the table 
+            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial;}");
+            client.println("table { border-collapse: collapse; width:35%; margin-left:auto; margin-right:auto; }");
+            client.println("th { padding: 12px; background-color: #0043af; color: white; }");
+            client.println("tr { border: 1px solid #ddd; padding: 12px; }");
+            client.println("tr:hover { background-color: #bcbcbc; }");
+            client.println("td { border: none; padding: 12px; }");
+            client.println(".sensor { color:white; font-weight: bold; background-color: #bcbcbc; padding: 1px; }");
+            
+            // Web Page Heading
+            client.println("</style></head><body><h1>ESP8266 with BME280</h1>");
+            client.println("<table><tr><th>MEASUREMENT</th><th>VALUE</th></tr>");
+            client.println("<tr><td>Temp. Celsius</td><td><span class=\"sensor\">");
+            client.println(bme.readTemperature());
+            client.println(" *C</span></td></tr>");  
+            client.println("<tr><td>Temp. Fahrenheit</td><td><span class=\"sensor\">");
+            client.println(1.8 * bme.readTemperature() + 32);
+            client.println(" *F</span></td></tr>");       
+            client.println("<tr><td>Pressure</td><td><span class=\"sensor\">");
+            client.println(bme.readPressure() / 100.0F);
+            client.println(" hPa</span></td></tr>");
+            client.println("<tr><td>Approx. Altitude</td><td><span class=\"sensor\">");
+            client.println(bme.readAltitude(SEALEVELPRESSURE_HPA));
+            client.println(" m</span></td></tr>"); 
+            client.println("<tr><td>Humidity</td><td><span class=\"sensor\">");
+            client.println(bme.readHumidity());
+            client.println(" %</span></td></tr>"); 
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}*/
+
+#include <ESP8266WiFi.h>
+
+// Set WiFi credentials
+#define WIFI_SSID "Ricny"
+#define WIFI_PASS "karoli111"
+
+void setup() {
+  // Setup serial port
+  Serial.begin(115200);
+  Serial.println();
+
+  // Begin WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  // Connecting to WiFi...
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  // Loop continuously while WiFi is not connected
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+
+  // Connected to WiFi
+  Serial.println();
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
 }
