014402f515c7e049ffa7a19ebf5da3ebfc62ae03
pokixrol <kaja.ricna@seznam.cz>
2022-12-05 17:08:56 +0100

    Výpis ve funkci, nahrazení delay funkcí milis

--- Vypisovani_hodnot_na_display/src/main.cpp
+++ Vypisovani_hodnot_na_display/src/main.cpp
@@ -12,6 +12,68 @@ Adafruit_BME280 bme;
 MQ135 mq135_sensor = MQ135(PIN_MQ135);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+void vypisHodnot()
+{
+  float tmp = bme.readTemperature();
+  float pressure = bme.readPressure() / 100.0F;
+  float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  float hum = bme.readHumidity();
+  float rzero = mq135_sensor.getRZero();
+  float correctedRZero = mq135_sensor.getCorrectedRZero(tmp, hum);
+  float resistance = mq135_sensor.getResistance();
+  float ppm = mq135_sensor.getPPM();
+  float correctedPPM = mq135_sensor.getCorrectedPPM(tmp, hum);
+  String text[9] = {"MQ135 RZero:  ", "Corrected RZero: ", "Resistance: ", "PPM: ", "Corrected PPM: ", "Temperature: ", "Pressure: ", "Altitude: ", "Humidity: "};
+  int i;
+  for (i = 0; i < 9; i++)
+  {
+    do
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(text[i]);
+      lcd.setCursor(0, 1);
+
+      switch (i)
+      {
+      case 0:
+        lcd.print(rzero);
+        break;
+      case 1:
+        lcd.print(correctedRZero);
+        break;
+      case 2:
+        lcd.print(resistance);
+        break;
+      case 3:
+        lcd.print(ppm);
+        break;
+      case 4:
+        lcd.print(correctedPPM);
+        break;
+      case 5:
+        lcd.print(tmp);
+        lcd.print(" *C");
+        break;
+      case 6:
+        lcd.print(pressure);
+        lcd.print(" hPa");
+        break;
+      case 7:
+        lcd.print(altitude);
+        lcd.print(" m");
+        break;
+      case 8:
+        lcd.print(hum);
+        lcd.print(" %");
+        break;
+      }
+      delay(2000);
+    } while (millis() <= 2000);
+  }
+  return;
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -28,58 +90,5 @@ void setup()
 
 void loop()
 {
-  float tmp = bme.readTemperature();
-  float pressure = bme.readPressure() / 100.0F;
-  float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  float hum = bme.readHumidity();
-  float rzero = mq135_sensor.getRZero();
-  float correctedRZero = mq135_sensor.getCorrectedRZero(tmp, hum);
-  float resistance = mq135_sensor.getResistance();
-  float ppm = mq135_sensor.getPPM();
-  float correctedPPM = mq135_sensor.getCorrectedPPM(tmp, hum);
-  String text[9] = {"MQ135 RZero:  ", "Corrected RZero: ", "Resistance: ", "PPM: ", "Corrected PPM: "
-  , "Temperature: ", "Pressure: ", "Altitude: ", "Humidity: "};
-int i;
-for ( i = 0; i < 9; i++)
-{
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(text[i]);
-  lcd.setCursor(0, 1);
-  switch (i)
-  {
-  case 0:
-    lcd.print(rzero);
-    break;
-  case 1:
-    lcd.print(correctedRZero);
-    break;
-  case 2:
-    lcd.print(resistance);
-    break;
-  case 3:
-    lcd.print(ppm);
-    break;
-  case 4:
-    lcd.print(correctedPPM);
-    break;
-  case 5:
-    lcd.print(tmp);
-    lcd.print(" *C");
-    break;
-  case 6:
-    lcd.print(pressure);
-    lcd.print(" hPa");
-    break;
-  case 7:
-    lcd.print(altitude);
-     lcd.print(" m");
-    break;
-  case 8:
-    lcd.print(hum);
-    lcd.print(" %");
-    break;
-  }
-  delay(2000);
-}
+  vypisHodnot();
 }
